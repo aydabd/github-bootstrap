b7120c0fa3cef3c0cd20fe295eff2a774248e461
Aydin.A <ayd.abd@gmail.com>
2026-09-09 12:47:23 +0200

    fix: allow personal app token for e2e dispatch
    Signed-off-by: Aydin.A <ayd.abd@gmail.com>

--- scripts/github-setup/test-app-auth-contract.sh
+++ scripts/github-setup/test-app-auth-contract.sh
@@ -39,7 +39,7 @@ assert_contains "AUTH_MODE=app-user bash ./scripts/github-setup/validate-app-aut
 assert_contains "echo \"::add-mask::\$APP_USER_REFRESH_TOKEN\"" "$resolver"
 assert_contains "case \"\$target_owner_type\" in" "$resolver"
 assert_contains "Organization)" "$resolver"
-assert_contains "repository-creation|repository-cleanup" "$resolver"
+assert_contains "repository-creation|repository-cleanup|e2e-dispatch" "$resolver"
 assert_contains "e2e-dispatch" "$resolver"
 assert_contains "mode=app-user" "$resolver"
 assert_contains "owner: \${{ inputs.app_owner }}" "$resolver"
